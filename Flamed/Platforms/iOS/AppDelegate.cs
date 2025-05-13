3871ff6ee816fc53de9a0fb9e371a2b633c7ffff
MerlinHose <hoeslmilan6@gmail.com>
2025-05-13 19:05:58 +0200

    Default

---
+++ Flamed/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace Flamed
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
